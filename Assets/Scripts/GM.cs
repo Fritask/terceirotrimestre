3945dd200aba36164903919fbc246bfe4d84e07d
Fritask <fritas.series@gmail.com>
2017-11-28 10:44:27 -0200

    Contando moedas

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -14,6 +14,10 @@ public class GM : MonoBehaviour {
 
 	public float timeToRespawn = 2f;
 
+	public UI ui;
+
+	GameData data = new GameData();
+
 	void Awake() {
 		if (instance == null) {
 			instance = this;
@@ -33,6 +37,15 @@ public class GM : MonoBehaviour {
 				player = obj.GetComponent<PlayerCtrl>();
 			}
 		}
+		DisplayHudData();
+	}
+
+	void DisplayHudData() {
+		ui.hud.txtCoinCount.text = "x " + data.coinCount;
+	}
+
+	public void IncrementCoinCount() {
+		data.coinCount++;
 	}
 
 	public void RespawnPlayer() {
